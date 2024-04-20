c89b2b1543af45fb690ef77c6e2c13f9a28e1b3a
tablemin03 <87592982+tablemin03@users.noreply.github.com>
2024-04-20 19:19:19 +0900

    [Gold V] Title: Hello World!, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13140. Hello World！/Hello World！.cc
+++ 백준/Gold/13140. Hello World！/Hello World！.cc
@@ -1,29 +1,34 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
-int alpha[10] = { 0,1,2,3,4,5,6,7,8,9 };
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	bool answer = false;
-	int n; cin >> n;
-	do {
-		if (alpha[2] == 0 || alpha[6] == 0) continue;
-		int tmp = (alpha[2] + alpha[6]) * 10000 + (alpha[1] + alpha[4]) * 1000 + (alpha[3] + alpha[5]) * 100 + alpha[3] * 20 + alpha[4] + alpha[0];
-		if (tmp == n) {
-			answer = true;
-			break;
+	int n = 0;
+	cin >> n;
+	for (int o = 0; o < 10; o++) {
+		for (int l = 0; l < 10; l++) {
+			for (int r = 0; r < 10; r++) {
+				for (int e = 0; e < 10; e++) {
+					for (int h = 1; h < 10; h++) {
+						for (int w = 1; w < 10; w++) {
+							for (int d = 0; d < 10; d++) {
+								if ((o + d + 20 * l + 100 * (l + r) + 1000 * (e + o) + 10000 * (h + w) == n)) {
+									if (o != l && o != r && o != e && o != h && o != w && o != d && l != r && l != e && l != h && l != w && l != d && r != h && r != w && r != d && r != e && e != h && e != w && e != d && h != w && h != d && w != d) {
+										cout << "  " << h << e << l << l << o << " \n"; //모든 변수가 서로 달라야함
+										cout << "+ " << w << o << r << l << d << " \n";
+										if (100000 <= n) {
+											cout << "------- \n " << n;
+											return 0;
+										}
+										cout << "------- \n  " << n;
+										return 0;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
-	} while (next_permutation(alpha, alpha + 10));
-	if (answer) {
-		cout << "  " << alpha[2] << alpha[1] << alpha[3] << alpha[3] << alpha[4] << "\n";
-		cout << "+ " << alpha[6] << alpha[4] << alpha[5] << alpha[3] << alpha[0] << "\n";
-		cout << "-------\n";
-		if (n >= 100000) cout << ' ' << n;
-		else cout << "  " << n;
 	}
-	else cout << "No Answer";
+	cout << "No Answer";
 }
