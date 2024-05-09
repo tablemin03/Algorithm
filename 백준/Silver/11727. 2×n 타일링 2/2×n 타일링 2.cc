18ef9abe3071740d2cf703ce40602db64f70699e
tablemin03 <87592982+tablemin03@users.noreply.github.com>
2024-05-09 10:43:19 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int d[1005];
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	d[1] = 1;
+	d[2] = 3;
+	int n; cin >> n;
+	for (int i = 3; i <= n; i++) {
+		d[i] = d[i - 1] + 2 * d[i - 2];
+		d[i] %= 10007;
+	}
+	cout << d[n];
+}
